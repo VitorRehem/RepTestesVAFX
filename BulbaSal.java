93da7b71579fb12f7b973dbf5059b67e677b9e89
pfredericoa <pfredericoa@gmail.com>
2026-09-14 19:26:24 -0300

    arquivos pokemons

--- BulbaSal.java
+++ BulbaSal.java
@@ -1,5 +1,10 @@
 public class BulbaSal extends Pokesal {
-    public BulbaSal(String nome, int hp, int atk, int def, int spd, TipoElemental tipo) {
-        super(nome, hp, atk, def, spd, tipo);
-    }
+    public BulbaSal
+    String nome
+    int hp
+    int atk 
+    int def
+    int spd
+    TipoELemental tipo
+    
 }
---
+++ SquritSal.java
@@ -0,0 +1,8 @@
+public class SquirtSal extends Pokesal {
+    public SquirtSal(
+    String nome
+    int hp 
+    int atk
+    int def
+    int spd
+    TipoElemental tipo
